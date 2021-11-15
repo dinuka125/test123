6f218fd0481ee227e8dd7058eda0586cb7105779
dinuka125 <47025217+dinuka125@users.noreply.github.com>
2021-11-15 20:13:00 +0530

    Update app.py
    modified the app

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello world ")
+import cv2
+from deepface import DeepFace
+
+img = cv2.imread("image1.jpg")# read the first image file
+img2 = cv2.imread("image2.jpg")# read the second image file
+
+result = DeepFace.verify(img,img2, model='facenet')
+
+print(result)
